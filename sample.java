51aa04686028248a0c04fc8d1f5930c2c49f3642
root <root@ip-172-31-40-184.ap-south-1.compute.internal>
2023-08-25 16:26:11 +0000

    3rd commit commit

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Rizwan pathan"); 
+        System.out.println("Hello, Rizwan pathan khan"); 
     }
 }
